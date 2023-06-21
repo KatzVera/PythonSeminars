8b9e1a801d7c7da91cf3b87900efdb80d2780ac6
Katz Vera <Katz.VG@gmail.com>
2023-06-21 17:19:04 +0300

    add new

--- Seminar2_4.py
+++ Seminar2_4.py
@@ -1,12 +1,29 @@
 # Задача 3: Дано число N. Заполните список длинной N элементами: 1, -3, 9, -27, 81, -243
 
-def zadacha_4():
-    numbers = []
-    N = int(input('Введите число: '))
+#def zadacha_4():
+    #numbers = []
+    #N = int(input('Введите число: '))
     
-    for el in range(N):
-        numbers.append((-3)**el) # append - команда добавляющая элемент в список
+    #for el in range(N):
+        #numbers.append((-3)**el) # append - команда добавляющая элемент в список
         # print(f'{el} -> {(-3)**el}')
-    print(numbers)
+    #print(numbers)
 
-zadacha_4()
+#zadacha_4()
+
+'''
+Задача 15: Программа на ввод принимает число N = равное кол-ву арбузов.
+Далее пользователь вводит массу каждого арбуза.
+Определить самый легкий и самый тяжелый арбуз.
+'''
+n = int(input('Введите количество арбузов: '))
+max_m = 0
+min_m = 10**100
+for i in range(n):
+    m = int(input('Введите вес арбуза: '))
+    if m > max_m:
+        max_m = m
+    if m < min_m:
+        min_m = m
+print(f'Самый тяжелый арбуз весит {max_m}')
+print(f'Самый легкий арбуз весит {min_m}')
